b514597539b8c2a512c9be6e66919344f1dcf830
ralexgt <romanalexrares@gmail.com>
2024-08-14 17:33:59 +0300

    14.08 - validate chirps length | app/api/admin routing

--- main.go
+++ main.go
@@ -14,15 +14,29 @@ func main() {
 	const port = "8080"
 
 	apiCfg := apiConfig{fileserverHits: 0}
-	serverMux := http.NewServeMux()
-	serverMux.Handle("/app/*", apiCfg.middlewareVisitsInc((http.StripPrefix("/app", http.FileServer(http.Dir(filepathToRoot))))))
-	serverMux.Handle("GET /healthz", middlewareLog(http.HandlerFunc(handlerReadiness)))
-	serverMux.Handle("GET /metrics", middlewareLog(http.HandlerFunc(apiCfg.handlerVisits)))
-	serverMux.Handle("GET /reset", middlewareLog(http.HandlerFunc(apiCfg.handlerReset)))
+
+	// middlewareStack := CreateMiddlewareStack(
+	// 	apiCfg.middlewareVisitsInc,
+	// 	middlewareLogging,
+	// )
+
+	router := http.NewServeMux()
+	fileServerHandler := apiCfg.middlewareVisitsAndLog((http.StripPrefix("/app", http.FileServer(http.Dir(filepathToRoot)))))
+
+	// app users routes
+	router.Handle("/app/*", fileServerHandler)
+
+	// admin routes
+	router.Handle("GET /admin/metrics", middlewareLogging(http.HandlerFunc(apiCfg.handlerVisits)))
+
+	// api routes
+	router.Handle("GET /api/healthz", middlewareLogging(http.HandlerFunc(handlerReadiness)))
+	router.Handle("GET /api/reset", middlewareLogging(http.HandlerFunc(apiCfg.handlerReset)))
+	router.Handle("POST /api/validate_chirp", middlewareLogging(http.HandlerFunc(handlerValidate)))
 
 	server := &http.Server{
 		Addr: "127.0.0.1:" + port,
-		Handler: serverMux,
+		Handler: router,
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathToRoot, port)
 	log.Fatal(server.ListenAndServe())
--- middleware.go
+++ middleware.go
@@ -3,18 +3,48 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
-func (cfg *apiConfig) middlewareVisitsInc(next http.Handler) http.Handler {
-	return  middlewareLog(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
+// type wrappedWriter struct {
+// 	http.ResponseWriter
+// 	statusCode int
+// }
+
+// func (w *wrappedWriter) wrappedHeader(statuscCode int) {
+// 	w.ResponseWriter.WriteHeader(statuscCode)
+// 	w.statusCode = statuscCode
+// }
+
+// type Middleware func(http.Handler) http.Handler
+
+// func CreateMiddlewareStack(middlewares ...Middleware) Middleware {
+// 	return func(next http.Handler) http.Handler {
+// 		for i := len(middlewares) - 1; i >= 0; i-- {
+// 			middleware := middlewares[i]
+// 			next = middleware(next)
+// 		}
+// 		return next
+// 	}
+// }
+
+func (cfg *apiConfig) middlewareVisitsAndLog(next http.Handler) http.Handler {
+	return  middlewareLogging(http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits++
 		next.ServeHTTP(w, r)
 	}))
 }
 
-func middlewareLog(next http.Handler) http.Handler {
+func middlewareLogging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.Path)
+		start := time.Now()
+
+		// wrapped := &wrappedWriter{
+		// 	ResponseWriter: w,
+		// 	statusCode: http.StatusOK,
+		// }
+
 		next.ServeHTTP(w, r)
+		log.Println(r.Method, r.URL.Path, time.Since(start))
 	})
 }
